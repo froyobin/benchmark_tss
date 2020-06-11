11413412d7c328fe90568a302f9b0163d69b18e9
Ford Betelgeuse <ford.betelgeusedent@gmail.com>
2020-06-11 19:59:33 +1000

    remove store files

--- storage/4/deployed_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "nameserver 8.8.8.8">>/etc/resolv.conf
-sleep 100
-go build ./cmd/tss/main.go ./cmd/tss/tss_http.go; echo "NjhlZWMzOTQ4ZjNjYmRlNWU1YTFjMzYyMTYwY2Q0YWU5NzBhZTA2ZTkwYmViYzhjNTI4YTc2MTdkMzg3NzhlNA==" | ./main -home /home/user/config -tss-port :8080 -peer /ip4/128.199.106.202/tcp/6668/ipfs/16Uiu2HAm4cFVFafrrP18JrVG8WT9xHgPA18jVfiAHfTuzDBKwBe5
-
--- storage/8/deployed_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "nameserver 8.8.8.8">>/etc/resolv.conf
-sleep 100
-go build ./cmd/tss/main.go ./cmd/tss/tss_http.go; echo "ODQ2NDg4MjQwMTAxNzE4ZTYyMWY5NzFhYjc3NWQzM2UzMTRkYjJlZWJlMmFkMDgwNjU2NDM1ZjMzNWQ2MzNhYg==" | ./main -home /home/user/config -tss-port :8080 -peer /ip4/128.199.106.202/tcp/6668/ipfs/16Uiu2HAm4cFVFafrrP18JrVG8WT9xHgPA18jVfiAHfTuzDBKwBe5
-
